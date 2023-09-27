383b90c99998fd0079ebed56bf171e544cfc1472
Albina Kovalchuk <alkadjin78@i.ua>
2023-09-27 16:22:42 +0300

    add/remove class animate for h1

--- js/menu.js
+++ js/menu.js
@@ -5,8 +5,10 @@
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
     menuList: document.querySelector('.mob-menu-list'),
+    animateTitle: document.querySelector('h1.hero-title'),
   };
 
+  console.log(refs.animateTitle);
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   refs.menuList.addEventListener('click', removeMenu);
@@ -14,9 +16,11 @@
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
+    refs.animateTitle.classList.toggle('animate__fadeInDown');
   }
   function removeMenu() {
     refs.menu.classList.add('is-hidden');
     refs.body.classList.remove('no-scroll');
+    refs.animateTitle.classList.add('animate__fadeInDown');
   }
 })();
